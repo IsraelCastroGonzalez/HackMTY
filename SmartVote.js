6f6e1eb7f0d494c7567ec6697e6dde6e22f1d2bf
Israel Castro <israel.casgon@gmail.com>
2015-03-21 18:39:25 -0600

    Page
    Made by Cesar

---
+++ SmartVote.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+	var control=0;
+	$("#adImage").click(function(){
+		if(control==0){
+			$(this).animate({top:"-=50px"},500);
+			$("#ad").append("<div class='adText'><a href='https://docs.google.com/forms/d/1biMMqtl-NbdwfPmGDQEqKuPsi0yLZrNTgmaivKOrgm0/viewform'><p id='adText'> ¿Sugerencias? Háznolas saber dando\nclick aqui</p></a></div>");
+			$(".adText").fadeIn(1000);
+			control++;
+
+		}
+		else{
+			$(this).animate({top:"+=50px"},500);
+			control--;
+		}
+		
+	});
+	$("#adImage").on("click",function(){
+		if(control==0){
+			$(".adText").fadeOut(2000).delay(1000).remove();
+			
+		}
+	});
+
+	function scrollToAnchor(aid){
+	    var aTag = $("a[name='"+ aid +"']");
+	    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+	}
+
+	$("#link1").click(function() {
+	   scrollToAnchor("inicio");
+	});
+
+
+	$("#link2").click(function() {
+	   scrollToAnchor("about");
+	});
+
+
+	$("#link3").click(function() {
+	   scrollToAnchor("video");
+	});
+
+
+	$("#link4").click(function() {
+	   scrollToAnchor("sponsor");
+	});
+	
+});
+
+
+
